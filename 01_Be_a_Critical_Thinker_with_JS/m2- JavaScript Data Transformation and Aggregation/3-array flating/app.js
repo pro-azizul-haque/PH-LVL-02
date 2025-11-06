f8b688f0b76eb0f76466559fa2fd633a5582ea3a
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-11-06 08:25:13 +0600

    add array flating

---
+++ 01_Be_a_Critical_Thinker_with_JS/m2- JavaScript Data Transformation and Aggregation/3-array flating/app.js
@@ -0,0 +1,14 @@
+// Creating a 3D array
+const cube = [
+  [
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [5, 6],
+    [7, 8],
+  ],
+];
+
+console.log(cube.flat(Infinity)); // Output: 4
+console.log( Infinity);
