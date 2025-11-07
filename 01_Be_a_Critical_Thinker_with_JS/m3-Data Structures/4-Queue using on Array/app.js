a65196e4176e65f47db16e4c3a8a1ef60b3473e2
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-11-07 11:01:52 +0600

    queue ds time complexity with method chain

--- 01_Be_a_Critical_Thinker_with_JS/m3-Data Structures/4-Queue using on Array/app.js
+++ 01_Be_a_Critical_Thinker_with_JS/m3-Data Structures/4-Queue using on Array/app.js
@@ -2,19 +2,23 @@ class Queue {
   constructor() {
     this.items = [];
   }
-
+  //? O(1)
   enqueue(value) {
     this.items.push(value);
   }
+  //? O(n) reason is every element of array shift place left
   dequeue() {
     return this.items.shift();
   }
+  //? O(1)
   peek() {
     return this.items[0];
   }
+  //? O(1)
   isEmpty() {
     return this.items.length === 0;
   }
+  //? O(1)
   get print() {
     console.log(this.items);
   }
