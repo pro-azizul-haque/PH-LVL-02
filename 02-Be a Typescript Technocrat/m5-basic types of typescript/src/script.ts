1d400ccdae449aaf5bacef71165ce5e33720fe51
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-11-13 19:55:20 +0600

    ts primitive and not primitive data types

--- 02-Be a Typescript Technocrat/m5-basic types of typescript/src/script.ts
+++ 02-Be a Typescript Technocrat/m5-basic types of typescript/src/script.ts
@@ -13,8 +13,26 @@
 // const users: string[] = ["a","b","c"]
 // console.log(users);
 // const users: [string,string,string] = ["a","b","as"]
-const users: (string | number)[] = ["a","b","as"]
-console.log(users);
-users.push(12)
-// users.push(true)
-console.log(users);
+// const users: (string | number)[] = ["a","b","as"]
+// console.log(users);
+// users.push(12)
+// // users.push(true)
+// console.log(users);
+
+
+const user:{
+    readonly name:string,
+    age :number,
+    isStudent:boolean,
+    profession:string
+} = {
+    name:"azizul",
+    age:18,
+    isStudent:true,
+    profession:"developer"
+}
+
+// user.age = "20"
+user.age = 20
+
+console.log(user);
