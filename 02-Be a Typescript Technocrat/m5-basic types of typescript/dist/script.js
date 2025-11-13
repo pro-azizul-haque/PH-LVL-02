14fd1dc9fe68efb6d8738a1c3773f0a20d9c65db
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-11-13 19:19:36 +0600

    intro to ts & compiler config

---
+++ 02-Be a Typescript Technocrat/m5-basic types of typescript/dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = "azizul";
+console.log(user);
+//# sourceMappingURL=script.js.map
---
+++ 02-Be a Typescript Technocrat/m5-basic types of typescript/src/script.ts
@@ -0,0 +1,2 @@
+const user: string = "azizul";
+console.log(user);
