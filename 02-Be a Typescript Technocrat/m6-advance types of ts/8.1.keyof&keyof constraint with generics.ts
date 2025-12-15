9c4b63060016ecd597ff041b2a63f9d1f9b30026
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-12-15 11:48:07 +0600

    keyof operator of typescript & uses to constraint for generics

---
+++ 02-Be a Typescript Technocrat/m6-advance types of ts/8.1.keyof&keyof constraint with generics.ts
@@ -0,0 +1,59 @@
+ 5  // keyof মানে holo একটা object type এর সব keys এর union।
+/**
+ * Summary (একদম short re-memorizable)
+keyof → object-এর সব key-এর union
+
+Used for:
+Safe property access
+Constraints
+Mapped types
+Utility types
+Strong generic APIs
+Formula মনে রাখো:
+keyof T = "prop1" | "prop2" | ...
+ */
+
+type Productx = {
+  name: string;
+  value: string;
+  brand: string;
+  price: number;
+};
+
+type ProductKeys = keyof Productx;
+
+const productx: ProductKeys = "price";
+// console.log(productx);
+
+type User = {
+  name: string;
+  id: number;
+};
+
+// function getProps(obj: User, key: "name"| "id") { // in this case the key is hard coded union type
+//   return obj[key];
+// }
+// function getProps(obj: User, key: keyof User) { // problem is its not generalized
+//   return obj[key];
+// }
+function getProps<T>(obj: T, key: keyof T) { // problem is its not generalized
+  return obj[key];
+}
+
+const user = {
+  name: "azizul",
+  id: 100,
+};
+
+console.log(getProps(user, "id"));
+
+const product1 = {
+  brand: "enovo",
+  price: 10000,
+  specs: {
+    cpu: "8core",
+    ram: "8GiB",
+  },
+};
+
+console.log(getProps(product1,"specs"));
