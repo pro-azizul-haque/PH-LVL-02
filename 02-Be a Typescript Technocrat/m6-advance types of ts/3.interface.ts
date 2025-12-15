e138c08355f836fe673b49baa75a4d1b3ab6e5aa
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-12-15 10:55:18 +0600

    uses of interface type

--- 02-Be a Typescript Technocrat/m6-advance types of ts/3.interface.ts
+++ 02-Be a Typescript Technocrat/m6-advance types of ts/3.interface.ts
@@ -1,4 +1,4 @@
-// interface holo  type alias er jat vai
+// interface holo  type alias er jat vai but it can merge automatically , extends others properties
 
 interface Product {
   id: number;
@@ -7,11 +7,27 @@ interface Product {
   industry: string;
 }
 
+// we can redefine Product interface but its merge with previous
+interface Product {
+  productionDate: number;
+  expireDate: number;
+}
+interface Chocolate {
+  color: string;
+  flavor: string;
+}
+// we can extends pros from another
+interface Product extends Chocolate {}
+
 const product1: Product = {
   id: 10,
   brand: "shera",
   price: 100,
   industry: "chemical",
+  productionDate: 1765774225318,
+  expireDate: 1765776625318,
+  color: "black",
+  flavor: "sweet",
 };
 
 console.log(product1);
