9eb844daab11146d8bb1b4169b5b19885d1a410a
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-12-15 11:39:36 +0600

    setting constrains for types

---
+++ 02-Be a Typescript Technocrat/m6-advance types of ts/8.constrains.ts
@@ -0,0 +1,33 @@
+/*Super-Simple Definition (memorizable)
+
+Constraint = Generic type-এর উপর boundary / condition
+ যাতে T must follow a specific shape, keys, primitive, or class.
+ */
+type Student = {
+  name: string;
+  age: string;
+  roll: number;
+};
+const isAuthorizedStudent = <
+  T extends // {
+  //   name: string;
+  //   age: string;
+  //   roll: number;
+  // }
+  Student,
+>(
+  identity: T,
+): boolean => {
+  if (identity) {
+    return true;
+  } else return false;
+};
+
+const student1 = {
+  name: "azizul",
+  age: "19",
+  roll: 10,
+  group: "Sci",
+  class: 10,
+};
+isAuthorizedStudent(student1);
